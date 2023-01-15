8e1d7eadfaea7864a29a2fe20fef71cda5246ec0
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-01-15 02:05:56 -0300

    ci: add missing variables in CI

--- terraform/github.tf
+++ terraform/github.tf
@@ -13,3 +13,15 @@ resource "github_actions_secret" "otp_secret" {
   secret_name     = "otp_secret"
   plaintext_value = var.otp_secret_dev_preview
 }
+
+resource "github_actions_secret" "otp_user" {
+  repository      = "reading-list"
+  secret_name     = "otp_user"
+  plaintext_value = var.otp_user
+}
+
+resource "github_actions_secret" "otp_service" {
+  repository      = "reading-list"
+  secret_name     = "otp_service"
+  plaintext_value = var.otp_service
+}
