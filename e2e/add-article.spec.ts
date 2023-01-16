b2b3ae407263b7744959ef30548fcb83eace857d
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-01-15 22:48:14 -0300

    tests: fix issues in e2e tests

--- e2e/add-article.spec.ts
+++ e2e/add-article.spec.ts
@@ -14,11 +14,9 @@ test.describe("AddArticle", () => {
     await page.getByLabel(/url/i).fill("https://example.com");
     await page.getByLabel(/tags/i).fill("Nextjs");
     await page.getByLabel(/tags/i).press("Enter");
-    await page.getByLabel(/otp/i).type(authenticator.generate(env.OTP_SECRET));
+    await page.getByLabel(/otp/i).fill(authenticator.generate(env.OTP_SECRET));
 
-    const navigationPromise = page.waitForNavigation({ url: BASE_URL });
     await page.getByRole("button", { name: /add$/i }).click();
-    await navigationPromise;
 
     expect(
       await page.getByRole("link", { name: /example domain/i }).count()
