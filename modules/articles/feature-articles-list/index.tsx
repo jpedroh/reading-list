05e1f1e66d75c4a85829ef45d1dcfce8af6ac016
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-04-18 18:59:05 -0300

    fix: revalidate articles list in every call

--- modules/articles/feature-articles-list/index.tsx
+++ modules/articles/feature-articles-list/index.tsx
@@ -1,6 +1,8 @@
 import { ArticlesList } from "./components/articles-list";
 import { fetchArticles } from "./services/fetch-articles";
 
+export const revalidate = 0;
+
 export async function ArticlesListEntrypoint() {
   const articles = await fetchArticles();
 
