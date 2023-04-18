7e3dcf14ff6ba8d9ada0218ce8fbbe7cd9ffd62e
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-04-18 18:49:04 -0300

    feat: use mutation API for adding new article

---
+++ app/api/article/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import { AddArticleSchema } from "../../../modules/articles/domain";
+import { addArticle } from "../../../modules/articles/feature-add-article/services/add-article";
+
+export async function POST(request: Request) {
+    const payload = AddArticleSchema.safeParse(await request.json());
+
+    if (!payload.success) {
+        return NextResponse.json({ message: "Bad Request", errors: payload.error }, { status: 400 });
+    }
+
+    try {
+        await addArticle(payload.data);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: error instanceof Error ? error.message : "Internal Server Error" }, { status: 500 });
+    }
+
+    return NextResponse.json({ message: "Success" });
+}
--- modules/articles/feature-add-article/components/add-article-form/index.tsx
+++ modules/articles/feature-add-article/components/add-article-form/index.tsx
@@ -1,13 +1,54 @@
-import { CreatableSelect } from "../../../../shared/components/creatable-select";
-import { Input } from "../../../../shared/components/input";
-import { fetchTags } from "../../services/fetch-tags";
-import styles from "./index.module.css";
+"use client"
 
-export async function AddArticleForm() {
-  const availableTags = await fetchTags();
+import { useRouter } from 'next/navigation';
+import { FormEvent, ReactNode, useState, useTransition } from 'react';
+
+import { Input } from "../../../../shared/components/input";
+import styles from "./index.module.css";
+import { AddArticleDto } from '../../../domain';
+
+async function submitArticle(payload: AddArticleDto) {
+  const response = await fetch(`api/article`, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  })
+  if (response.status >= 400) {
+    throw new Error((await response.json()).message)
+  }
+}
+
+export function AddArticleForm({ children, onCreated }: { children: ReactNode, onCreated: () => {} }) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const isMutating = isFetching || isPending;
+
+  async function handleSubmit(evt: FormEvent<HTMLFormElement>) {
+    try {
+      evt.preventDefault();
+
+      console.log('testing')
+      setErrorMessage('');
+      setIsFetching(true);
+      await submitArticle(Object.fromEntries(new FormData(evt.target as HTMLFormElement)))
+      setIsFetching(false);
+      onCreated();
+      console.log('testing')
+
+      startTransition(() => {
+        router.refresh();
+      });
+    } catch (error) {
+      console.log(error)
+      setErrorMessage(error.message)
+      setIsFetching(false);
+    }
+  }
 
   return (
-    <form className={styles.container} method="POST" action="/api/article">
+    <form className={styles.container} onSubmit={handleSubmit}>
       <label>
         <span>URL</span>
         <Input
@@ -20,12 +61,7 @@ export async function AddArticleForm() {
 
       <label>
         <span>Tags</span>
-        <CreatableSelect
-          name="tags"
-          required
-          isMulti={true}
-          options={availableTags}
-        />
+        {children}
       </label>
 
       <label>
@@ -33,7 +69,8 @@ export async function AddArticleForm() {
         <Input name="otp" type={"text"} required placeholder="000000" />
       </label>
 
-      <button type="submit">Add</button>
+      {errorMessage && <p role="alert" className='bg-red-700 text-white border border-red-900 p-3 rounded'>{errorMessage}</p>}
+      <button disabled={isMutating} type="submit">{isMutating ? "Loading" : "Add"}</button>
     </form>
   );
 }
