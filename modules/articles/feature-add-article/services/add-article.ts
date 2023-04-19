6628ecf247db12ed0dd2e3569c392c8d399a0391
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-04-18 21:55:49 -0300

    style: format code

--- modules/articles/feature-add-article/services/add-article.ts
+++ modules/articles/feature-add-article/services/add-article.ts
@@ -1,7 +1,12 @@
 import { randomUUID } from "crypto";
 import parse from "node-html-parser";
 import { authenticator } from "otplib";
-import { articles, articleTags, db, NewArticle } from "../../../shared/database";
+import {
+  articles,
+  articleTags,
+  db,
+  NewArticle,
+} from "../../../shared/database";
 import { env } from "../../../shared/env";
 import { AddArticleDto } from "../../domain";
 
@@ -10,11 +15,14 @@ export async function addArticle(data: AddArticleDto) {
     throw new Error("Invalid OTP provided");
   }
 
-  return saveArticle({
-    id: randomUUID(),
-    title: await getTitleFromUrl(data.url),
-    url: data.url,
-  }, data.tags);
+  return saveArticle(
+    {
+      id: randomUUID(),
+      title: await getTitleFromUrl(data.url),
+      url: data.url,
+    },
+    data.tags
+  );
 }
 
 function isOtpValid(token: string) {
@@ -37,12 +45,14 @@ async function saveArticle(article: NewArticle, tags: string[]) {
   try {
     return await db.transaction(async (tx) => {
       await tx.insert(articles).values({ ...article, addedAt: new Date() });
-      await tx.insert(articleTags).values(tags.map(tag => {
-        return { articleId: article.id, tag };
-      }));
+      await tx.insert(articleTags).values(
+        tags.map((tag) => {
+          return { articleId: article.id, tag };
+        })
+      );
     });
   } catch (cause) {
-    console.error(cause)
+    console.error(cause);
     throw new Error("Internal server error", { cause });
   }
 }
