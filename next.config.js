4c639835b34130e3ea0dd5d5f771e564891c8530
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-06-28 12:34:20 -0300

    chore: remove unused dependecy in pg

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@
 const nextConfig = {
   experimental: {
     serverActions: true,
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
-    );
-    return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
