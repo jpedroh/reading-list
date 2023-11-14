7299bbde9669b6a739636e420da5d82e56400f31
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-11-13 23:48:06 -0300

    chore: Tweak nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+  typescript: {
+    ignoreBuildErrors: true
   },
   eslint: {
     ignoreDuringBuilds: true
