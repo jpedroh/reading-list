205641684644e9654e4e73a8702206069e299249
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-27 23:49:25 -0300

    refactor: use react-aria-components link

--- modules/articles/feature-articles-list/components/article-row/styles.tsx
+++ modules/articles/feature-articles-list/components/article-row/styles.tsx
@@ -1,8 +1,11 @@
+import { Link as RACLink } from "react-aria-components";
 import { twc } from "react-twc";
 
 export const Container = twc.li`border border-zinc-700 bg-zinc-800 bg-opacity-30 hover:bg-opacity-100 transition-all duration-300 rounded-xl p-4 flex flex-col gap-4 relative focus-within:ring-2 focus-within:ring-blue-600`;
 
-export const Title = twc.a`text-xl font-bold after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 outline-none`;
+export const Title = twc(
+  RACLink,
+)`text-xl font-bold after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 outline-none`;
 
 export const Info = twc.div`flex items-center justify-between text-zinc-400 overflow-auto`;
 
