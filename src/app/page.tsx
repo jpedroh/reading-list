451b3c2d36b7583e53d0a09dbd4c4e93a01b5766
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-28 00:36:04 -0300

    refactor: add content base component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { ArticlesDisplay } from "@reading-list/modules/articles/articles-display/server";
 import { Button } from "@reading-list/modules/shared/ui";
-import { Header } from "@reading-list/modules/shared/ui/server";
+import { Content, Header } from "@reading-list/modules/shared/ui/server";
 import Link from "next/link";
-import { twc } from "react-twc";
 
 export const runtime = "edge";
 
@@ -11,8 +10,6 @@ export const dynamic = "force-dynamic";
 export const metadata = {
   title: "Reading List",
 };
-
-const Content = twc.div`flex flex-col gap-8 w-full md:grid md:grid-cols-[300px_1fr]`;
 
 export default function Home() {
   return (
@@ -23,9 +20,9 @@ export default function Home() {
           <Link href={"/add-article"}>Add new article</Link>
         </Button>
       </Header.Root>
-      <Content>
+      <Content.Root>
         <ArticlesDisplay />
-      </Content>
+      </Content.Root>
     </>
   );
 }
