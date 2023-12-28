880ea924162deab3382d0abec2409d6ea8acec75
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-27 22:23:06 -0300

    fix: type issues after refactor

--- src/modules/shared/ui/src/components/button/index.tsx
+++ src/modules/shared/ui/src/components/button/index.tsx
@@ -1,6 +1,9 @@
-import { Button as RACButton } from "react-aria-components";
+import {
+  Button as RACButton,
+  ButtonProps as RACButtonProps,
+} from "react-aria-components";
 import { twc } from "react-twc";
 
-export const Button = twc(
-  RACButton,
-)`rounded-md bg-blue-800 bg-opacity-50 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 disabled:opacity-40`;
+export const Button = twc(RACButton)<
+  RACButtonProps & { asChild?: boolean }
+>`rounded-md bg-blue-800 bg-opacity-50 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 disabled:opacity-40`;
