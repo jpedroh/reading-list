da2350d2f94d92feb88ff40be1dad357b60d0ad7
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-28 02:10:30 -0300

    refactor: use shared input component

--- src/modules/articles/articles-filter/src/views/search-term/index.tsx
+++ src/modules/articles/articles-filter/src/views/search-term/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Input } from "@reading-list/modules/shared/ui";
 import { useArticlesFilter } from "@reading-list/modules/shared/articles-filter-provider";
 import { useId } from "react";
 
@@ -12,10 +13,8 @@ export function SearchTermFilter() {
       <label className="sr-only" id={searchTermTitleId}>
         Search by term
       </label>
-      <input
-        type="text"
+      <Input
         aria-labelledby={searchTermTitleId}
-        className={`border rounded-lg p-3 w-full border-zinc-700 bg-zinc-800 bg-opacity-30 hover:bg-opacity-100 focus:bg-opacity-100 transition-all duration-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-blue-600 outline-none`}
         placeholder="Search"
         value={searchTerm}
         onChange={(evt) => setSearchTerm(evt.target.value)}
