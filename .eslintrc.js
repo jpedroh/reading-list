7565cceacbf9f835732ddbf790bdd44350583d20
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-28 01:08:58 -0300

    chore: add eslint rules configuration

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["@nx"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
+      rules: {
+        "@nx/enforce-module-boundaries": [
+          "error",
+          {
+            allow: [],
+            depConstraints: [
+              {
+                sourceTag: "*",
+                onlyDependOnLibsWithTags: ["*"],
+              },
+              {
+                sourceTag: "type:feature",
+                onlyDependOnLibsWithTags: ["type:shared"],
+              },
+              {
+                sourceTag: "type:shared",
+                onlyDependOnLibsWithTags: ["type:shared"],
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+};
