2b142667ddfd150c99d7c7278b2ed1162a7a8763
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-12-29 13:12:49 -0300

    ci: use serve script to run e2e test (#344)

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   globalTeardown: require.resolve("./e2e/fixtures/clear-database"),
   fullyParallel: Boolean(process.env.CI),
   webServer: {
-    command: "pnpm run start",
+    command: "pnpm run serve",
     url: "http://127.0.0.1:3000",
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
