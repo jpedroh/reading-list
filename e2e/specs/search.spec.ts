f013a7ba56537befc78ccd8c592b3407b0abb29d
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-29 13:20:05 -0300

    chore: randomize title to avoid test interference

--- e2e/specs/search.spec.ts
+++ e2e/specs/search.spec.ts
@@ -1,20 +1,24 @@
 import { expect, test } from "@playwright/test";
 import { db, articles } from "@reading-list/modules/shared/database";
+import { randomBytes } from "crypto";
 
 test.describe("Search", () => {
   test("if there's an article with a title that contains the search string, it appears on the results", async ({
     page,
   }) => {
+    const randomTitle = randomBytes(64).toString("base64");
+    const anotherRandomTitle = randomBytes(64).toString("base64");
+
     await db.insert(articles).values([
       {
         id: crypto.randomUUID(),
-        title: "A simple Example Title",
+        title: randomTitle,
         url: "https://example.com",
         addedAt: new Date(),
       },
       {
         id: crypto.randomUUID(),
-        title: "A completely different Title",
+        title: anotherRandomTitle,
         url: "https://example.com",
         addedAt: new Date(),
       },
@@ -22,9 +26,9 @@ test.describe("Search", () => {
 
     await page.goto("/");
 
-    const articleOne = page.getByRole("link", { name: /example title/i });
+    const articleOne = page.getByRole("link", { name: randomTitle });
     const articleTwo = page.getByRole("link", {
-      name: /completely different/i,
+      name: anotherRandomTitle,
     });
 
     await expect(articleOne).toBeAttached();
