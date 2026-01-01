f8a0fe00e692739e8cc3d6e406e98f721e1145f1
Joao Duarte <joao.pedro.hsd@gmail.com>
2026-01-01 14:13:25 -0300

    feat: initial work

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,32 +6,24 @@ import {
   HeaderTitle,
 } from "@reading-list/modules/shared/ui";
 import { Content } from "@reading-list/modules/shared/ui/server";
-import Link from "next/link";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const runtime = "edge";
+export const Route = createFileRoute("/")({
+  component: Home,
+});
 
-export const dynamic = "force-dynamic";
-
-export const metadata = {
-  title: "Reading List",
-};
-
-export default function Home() {
+function Home() {
   return (
     <>
       <HeaderRoot>
         <HeaderTitle>My Reading List</HeaderTitle>
         <Button asChild>
-          <Link href={"/add-article"}>Add new article</Link>
+          {/* <Link href={"/add-article"}>Add new article</Link> */}
         </Button>
       </HeaderRoot>
       <Content.Root>
-        <Content.Aside>
-          <ArticlesFilter />
-        </Content.Aside>
-        <Content.Main>
-          <ArticlesList />
-        </Content.Main>
+        <Content.Aside>{/* <ArticlesFilter /> */}</Content.Aside>
+        <Content.Main>{/* <ArticlesList /> */}</Content.Main>
       </Content.Root>
     </>
   );
