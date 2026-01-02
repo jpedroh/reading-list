a11ee106ebbd9c2be065d07697f94c28197324fe
Joao Duarte <joao.pedro.hsd@gmail.com>
2026-01-01 22:16:02 -0300

    chore: adjusting

--- packages/shared-articles-filter-provider/src/index.ts
+++ packages/shared-articles-filter-provider/src/index.ts
@@ -1,10 +1,17 @@
 import { useAtom } from "jotai";
 import { atomWithHash } from "jotai-location";
 
-const searchTermAtom = atomWithHash<string>("searchTerm", "");
-const tagsAtom = atomWithHash<string[]>("tags", []);
+const searchTermAtom = atomWithHash("searchTerm", "");
+const tagsAtom = atomWithHash("tags", new Array<string>());
 
-export function useArticlesFilter() {
+type UseArticlesFilter = {
+  searchTerm: string;
+  tags: string[];
+  setSearchTerm: (x: string) => void;
+  setTags: (x: string[]) => void;
+};
+
+export function useArticlesFilter(): UseArticlesFilter {
   const [searchTerm, setSearchTerm] = useAtom(searchTermAtom);
   const [tags, setTags] = useAtom(tagsAtom);
 
